6f4b26420ef2e2c50216a8213698256738871135
Olga Mikhnenko <olga230397@icloud.com>
2021-02-10 13:28:10 +0300

    Initial Commit

---
+++ MVVM with fake NetworkManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM with fake NetworkManager
+//
+//  Created by Helga on 10.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
